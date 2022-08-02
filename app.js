8ebc190f96bcc37224977a035f1546f72574ce57
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-02 19:43:18 +0300

    cache the DOM

---
+++ app.js
@@ -0,0 +1,29 @@
+//caching the DOM
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
+const divide = document.querySelector('.divide')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const multiply = document.querySelector('.multiply')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const plus = document.querySelector('.plus')
+const zero = document.querySelector('.zero')
+const dot = document.querySelector('.dot')
+const equal = document.querySelector('.equal')
+const minus = document.querySelector('.minus')
+
+
+class calculator {
+    //operations
+    sum(a, b){
+        return a + b;
+    }
+    divide(a, b){
+        return a / b;
+    }
+}
+
